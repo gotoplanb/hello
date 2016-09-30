7b2961b43d24ace7285fa36370e5460cee3017e0
Dave Stanton <dstanton@appirio.com>
2016-09-30 16:50:43 -0400

    Run on any port.

--- javascript/node-express/index.js
+++ javascript/node-express/index.js
@@ -2,8 +2,12 @@ var express = require('express')
 
 var app = express()
 
+app.set('port', process.env.PORT || 3000)
+
 app.get('/', function(req, res) {
   res.json({message: "Hello world"})
 })
 
-app.listen(3000)
+app.listen(app.get('port'), function() {
+ console.log('Server started on localhost:' + app.get('port') + '; Press Ctrl-C to terminate.');
+});

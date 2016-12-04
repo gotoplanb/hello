750e27c3277d31e8ef9c23d55a3728b8999e6aec
Dave Stanton <davestanton.us@gmail.com>
2016-12-04 07:34:00 -0500

    Hello Mocha.

--- javascript/node-express/index.js
+++ javascript/node-express/index.js
@@ -5,8 +5,12 @@ var app = express()
 app.set('port', process.env.PORT || 3000)
 
 app.get('/', function(req, res) {
-  res.json({message: "Hello " + req.query.name})
-})
+  if (typeof req.query.name != 'undefined') {
+    res.json({message: "Hello, " + req.query.name + "!"});
+  } else {
+    res.json({message: "Hello, World!"});
+  }
+});
 
 app.listen(app.get('port'), function() {
  console.log('Server started on localhost:' + app.get('port') + '; Press Ctrl-C to terminate.');

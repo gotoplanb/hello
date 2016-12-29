f25681d8326a816ea84bb57bc9f431ffaa8989db
Dave Stanton <davestanton.us@gmail.com>
2016-12-29 14:55:20 -0500

    Set default state for HelloWorld greeting.

--- javascript/create-react-app/hello-world/src/HelloWorld.js
+++ javascript/create-react-app/hello-world/src/HelloWorld.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import './HelloWorld.css';
 
 class HelloWorld extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { greeting: 'Hello' };
+  }
   render() {
     return (
-      <div className="HelloWorld">Hello {this.props.name}!</div>
+      <div className="HelloWorld">
+        {this.state.greeting} {this.props.name}!
+      </div>
     );
   }
 }

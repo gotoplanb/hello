383e86f15361849d49e7523b4e244fcb61698298
Dave Stanton <davestanton.us@gmail.com>
2017-01-02 17:32:44 -0500

    WIP testing HelloWorldList.

---
+++ javascript/create-react-app/hello-world/src/HelloWorldList.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import HelloWorldList from './HelloWorldList';
+import AddGreeter from './AddGreeter';
+import HelloWorld from './HelloWorld';
+
+describe(HelloWorldList, () => {
+  const component = shallow(
+    <HelloWorldList />
+  );
+
+  it('renders and matches our snapshot', () => {
+    const component = renderer.create(
+      <HelloWorldList />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('contains an AddGreeter subcomponent', () => {
+    expect(component.find(AddGreeter)).toHaveLength(1);
+  });
+
+});

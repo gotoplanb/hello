b7017bf0710e453a5caba0a6145be03dbc94c448
Dave Stanton <davestanton.us@gmail.com>
2017-01-03 15:53:05 -0500

    Storybook use a decorator to set maxWidth.

--- javascript/create-react-app/hello-world/src/HelloWorldList.stories.js
+++ javascript/create-react-app/hello-world/src/HelloWorldList.stories.js
@@ -3,6 +3,11 @@ import { storiesOf, action, linkTo } from '@kadira/storybook';
 import HelloWorldList from './HelloWorldList';
 
 storiesOf('HelloWorldList', module)
+  .addDecorator((story) => (
+    <div style={{maxWidth: '600px'}}>
+      {story()}
+    </div>
+  ))
   .add('default', () => (
     <HelloWorldList />
   ))

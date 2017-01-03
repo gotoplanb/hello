f4ad2437b6e71099d0136fedd47f73b119e89bac
Dave Stanton <davestanton.us@gmail.com>
2017-01-03 14:51:41 -0500

    Storybook HelloWorld.

--- javascript/create-react-app/hello-world/src/stories/index.js
+++ javascript/create-react-app/hello-world/src/stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
 import Button from './Button';
 import Welcome from './Welcome';
+import HelloWorld from '../HelloWorld';
 
 storiesOf('Welcome', module)
   .add('to Storybook', () => (
@@ -14,4 +15,15 @@ storiesOf('Button', module)
   ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ));
+
+storiesOf('HelloWorld', module)
+  .add('with short name', () => (
+    <HelloWorld name="Dave" />
+  ))
+  .add('with long name', () => (
+    <HelloWorld name="Daveapotomus" />
+  ))
+  .add('with some emoji', () => (
+    <HelloWorld name="😀 😎 👍 💯" />
   ));
